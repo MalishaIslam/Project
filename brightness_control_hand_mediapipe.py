170ca7ba02f24067e556bf25275de321dfe2504e
--oditiya@gmail.com <malishatapotee@gmail.com>
2021-08-03 13:32:53 +0600

    read frame( webcam )

--- brightness_control_hand_mediapipe.py
+++ brightness_control_hand_mediapipe.py
@@ -15,3 +15,22 @@ engine = pyttsx3.init()
 engine.say('Dear, Turukka, I am here')
 engine.say('Turn on the webcam')
 engine.runAndWait()
+
+
+with mp_hands.Hands(
+    min_detection_confidence = 0.7,
+    min_tracking_confidence = 0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)

8408ba01054d0a6b673987715f6748f2e0590b86
--oditiya@gmail.com <malishatapotee@gmail.com>
2021-08-03 13:31:32 +0600

    Video Capture & text to speech

--- brightness_control_hand_mediapipe.py
+++ brightness_control_hand_mediapipe.py
@@ -8,3 +8,10 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 drawing_styles = mp.solutions.drawing_styles
 
+cap = cv2.VideoCapture(0)
+#fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+#writer = cv2.VideoWriter('D:\zoom/tt.avi',cv2.VideoWriter_fourcc('X','V','I','D'), 30, (640,480))
+engine = pyttsx3.init()
+engine.say('Dear, Turukka, I am here')
+engine.say('Turn on the webcam')
+engine.runAndWait()

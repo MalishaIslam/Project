30aaefd0015436211e81309ff9159f303c93da3f
--oditiya@gmail.com <malishatapotee@gmail.com>
2021-08-03 13:27:01 +0600

    Module Initialization

---
+++ brightness_control_hand_mediapipe.py
@@ -0,0 +1,10 @@
+import cv2
+import mediapipe as mp
+import math, numpy as np
+import screen_brightness_control as sbc
+import pyttsx3
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+drawing_styles = mp.solutions.drawing_styles
+
